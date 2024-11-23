862615e7def33c12d133b96a939a71e71f22de7b
Raphael Coelho <raphael.phael@gmail.com>
2024-11-23 15:51:12 -0300

    fix: corrige paths em constantes.py

--- src/core/constants.py
+++ src/core/constants.py
@@ -6,12 +6,13 @@ Constantes:
 - FILE_PATH: Caminho absoluto do arquivo atual.
 - PROJECT_PATH: Caminho absoluto da raiz do projeto.
 - LOG_FOLDER_PATH: Caminho absoluto da pasta de logs.
-- SRC_FOLDER_PATH: Caminho absoluto da pasta de código-fonte.
-- CORE_FOLDER_PATH: Caminho absoluto da pasta 'core' dentro da pasta de código-fonte.
+- SRC_FOLDER_PATH: Caminho absoluto da pasta de código fonte.
+- CONFIG_FOLDER_PATH: Caminho absoluto da pasta de configurações.
+- CORE_FOLDER_PATH: Caminho absoluto da pasta core do projeto.
 - LOG_FILE_PATH: Caminho absoluto do arquivo de logs.
 - DOCS_YAML_FILE_PATH: Caminho absoluto do arquivo YAML contendo as docstrings.
-- DOCSTRINGMANAGER_FILE_PATH: Caminho absoluto do arquivo 'docstringmanager.py'.
-- TEST_FILE_PATH: Caminho absoluto do arquivo 'test.py'.
+- DOCSTRINGMANAGER_FILE_PATH: Caminho absoluto do arquivo docstringmanager.py.
+- SAMPLE_FILE_PATH: Caminho absoluto do arquivo de exemplo.
 """
 from pathlib import Path
 
@@ -22,10 +23,11 @@ PROJECT_PATH = FILE_PATH.parents[2]
 # Atribui as constantes de pastas
 LOG_FOLDER_PATH = PROJECT_PATH / 'logs'
 SRC_FOLDER_PATH = PROJECT_PATH / 'src'
+CONFIG_FOLDER_PATH = PROJECT_PATH / 'config'
 CORE_FOLDER_PATH = SRC_FOLDER_PATH / 'core'
 
 # Atribui as constantes de arquivos
 LOG_FILE_PATH = LOG_FOLDER_PATH / 'app.log'
-DOCS_YAML_FILE_PATH = SRC_FOLDER_PATH / 'docstringmanager.yaml'
+DOCS_YAML_FILE_PATH = CONFIG_FOLDER_PATH / 'docstringmanager.yaml'
 DOCSTRINGMANAGER_FILE_PATH = SRC_FOLDER_PATH / 'docstringmanager.py'
-TEST_FILE_PATH = SRC_FOLDER_PATH / 'test.py'
+SAMPLE_FILE_PATH = SRC_FOLDER_PATH / 'sample.py'
--- src/usage_examples.py
+++ src/usage_examples.py
@@ -1,5 +1,16 @@
+"""
+Script de demonstração para gerenciar docstrings em scripts Python.
+
+Uso:
+- Altere o argumento `chosen_example` na função `main` para definir a operação:
+    - `1`: Adicionar docstrings a partir de um arquivo YAML.
+    - `2`: Remover docstrings específicas definidas em um arquivo YAML.
+    - `3`: Remover todas as docstrings de um script Python.
+"""
+
+
 from docstringmanager import add_docstrings_from_yaml, remove_docstrings
-from core.constants import DOCS_YAML_FILE_PATH, TEST_FILE_PATH
+from core.constants import DOCS_YAML_FILE_PATH, SAMPLE_FILE_PATH
 from core.logger import logger
 
 
@@ -7,17 +18,17 @@ def main(chosen_example: int = 1):
     # Exemplo 1: Adiciona docstrings a um arquivo Python a partir de um arquivo YAML
     if chosen_example == 1:
         logger.info('Adicionando docstrings...')
-        add_docstrings_from_yaml(TEST_FILE_PATH, DOCS_YAML_FILE_PATH)
+        add_docstrings_from_yaml(SAMPLE_FILE_PATH, DOCS_YAML_FILE_PATH)
        
     # Exemplo 2: Remove docstrings de arquivo Python a partir de um arquivo YAML 
     if chosen_example == 2:
         logger.info('Removendo docstrings...')
-        remove_docstrings(TEST_FILE_PATH, DOCS_YAML_FILE_PATH)
+        remove_docstrings(SAMPLE_FILE_PATH, DOCS_YAML_FILE_PATH)
 
     # Exemplo 3: Remove todas as docstrings de um arquivo Python
     if chosen_example == 3:
         logger.info('Removendo todas as docstrings...')
-        remove_docstrings(TEST_FILE_PATH)
+        remove_docstrings(SAMPLE_FILE_PATH)
 
     # Exibe mensagem de conclusão
     if chosen_example:
